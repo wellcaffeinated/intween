b3b9366ec0ce64b87c619632760e8c56e9726bf5
Jasper <well.caffeinated@gmail.com>
2021-09-15 00:19:47 -0600

    add google analytics to docs site

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,8 @@
 const path = require('path')
 const fs = require('fs')
 
+const GTAG_ID = 'G-9TYR614DDH'
+
 const apidir = path.join(__dirname, '../api')
 const apiFiles = fs.readdirSync(apidir).filter(file => {
   return file !== 'README.md' && file.substr(-3) === '.md' && !fs.lstatSync(path.join(apidir, file)).isDirectory()
@@ -32,6 +34,22 @@ const sidebar = {
 module.exports = {
   title: 'InTween',
   description: 'Your companion for building rich interactive media with Interactive Tweens',
+  head: [
+    [
+      'script',
+      {
+        async: true,
+        src: `https://www.googletagmanager.com/gtag/js?id=${GTAG_ID}`,
+      },
+    ],
+    [
+      'script',
+      {},
+      [
+        `window.dataLayer = window.dataLayer || [];\nfunction gtag(){dataLayer.push(arguments);}\ngtag('js', new Date());\ngtag('config', '${GTAG_ID}');`,
+      ],
+    ],
+  ],
   themeConfig: {
     nav: [
       { text: 'Guide', link: '/guide/' },
